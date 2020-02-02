7ec503a15cc9fb6dcff609593f86d15103b4a3c3
augusts7 <dahal_augus@hotmail.com>
2020-02-01 22:06:22 -0600

    New todo application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {TodoService} from "./todo.service";
 
 import { AppComponent } from './app.component';
+import {TodoListComponent} from "./todo-list.component";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
